4317e430c80ec093ee10b029541e17472ab8c4d0
Ilya Petrov <ilya.muromec@gmail.com>
2014-11-29 16:16:48 +0700

    style

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ var Subst = function (data, mem) {
 
 var Gost = function (sbox) {
     var mem = new global.Uint32Array(1162);
-    
+
     this.k = mem.slice(0, 8);
     this.k87 = mem.slice(8, 264);
     this.k65 = mem.slice(264, 520);
@@ -40,50 +40,49 @@ Gost.prototype.boxinit = function (sbox) {
 
     for (i = 0; i < 256; i++) {
         this.k87[i] = (sbox.k8[i>>>4] <<4 | sbox.k7 [i &15])<<24;
-		this.k65[i] = (sbox.k6[i>>>4] << 4 | sbox.k5 [i &15])<<16;
-		this.k43[i] = (sbox.k4[i>>>4] <<4  | sbox.k3 [i &15])<<8;
-		this.k21[i] = sbox.k2[i>>>4] <<4  | sbox.k1 [i &15];
+        this.k65[i] = (sbox.k6[i>>>4] << 4 | sbox.k5 [i &15])<<16;
+        this.k43[i] = (sbox.k4[i>>>4] <<4  | sbox.k3 [i &15])<<8;
+        this.k21[i] = sbox.k2[i>>>4] <<4  | sbox.k1 [i &15];
     }
 };
 
 Gost.prototype.pass = function (x) {
-    x = this.k87[x>>>24 & 255] | this.k65[x>>>16 & 255]| 
-		this.k43[x>>> 8 & 255] | this.k21[x & 255]; 
-	/* Rotate left 11 bits */ 
-	return x<<11 | x>>>(32-11);
+    x = this.k87[x>>>24 & 255] | this.k65[x>>>16 & 255]|
+        this.k43[x>>> 8 & 255] | this.k21[x & 255];
+    /* Rotate left 11 bits */
+    return x<<11 | x>>>(32-11);
 };
 
 Gost.prototype.crypt64 = function (clear, out) {
     var n = this.n;
-	n[0] = clear[0]|(clear[1]<<8)|(clear[2]<<16)|(clear[3]<<24); 
-	n[1] = clear[4]|(clear[5]<<8)|(clear[6]<<16)|(clear[7]<<24); 
-	/* Instead of swappclearg halves, swap names each round */ 
+    n[0] = clear[0]|(clear[1]<<8)|(clear[2]<<16)|(clear[3]<<24);
+    n[1] = clear[4]|(clear[5]<<8)|(clear[6]<<16)|(clear[7]<<24);
+    /* Instead of swappclearg halves, swap names each round */
 
-	n[1] ^= this.pass(n[0]+this.k[0]); n[0] ^= this.pass(n[1]+this.k[1]); 
-	n[1] ^= this.pass(n[0]+this.k[2]); n[0] ^= this.pass(n[1]+this.k[3]); 
-	n[1] ^= this.pass(n[0]+this.k[4]); n[0] ^= this.pass(n[1]+this.k[5]); 
-	n[1] ^= this.pass(n[0]+this.k[6]); n[0] ^= this.pass(n[1]+this.k[7]); 
-  
-	n[1] ^= this.pass(n[0]+this.k[0]); n[0] ^= this.pass(n[1]+this.k[1]);
-	n[1] ^= this.pass(n[0]+this.k[2]); n[0] ^= this.pass(n[1]+this.k[3]);
-	n[1] ^= this.pass(n[0]+this.k[4]); n[0] ^= this.pass(n[1]+this.k[5]);
-	n[1] ^= this.pass(n[0]+this.k[6]); n[0] ^= this.pass(n[1]+this.k[7]);
-                               
-	n[1] ^= this.pass(n[0]+this.k[0]); n[0] ^= this.pass(n[1]+this.k[1]);
-	n[1] ^= this.pass(n[0]+this.k[2]); n[0] ^= this.pass(n[1]+this.k[3]);
-	n[1] ^= this.pass(n[0]+this.k[4]); n[0] ^= this.pass(n[1]+this.k[5]);
-	n[1] ^= this.pass(n[0]+this.k[6]); n[0] ^= this.pass(n[1]+this.k[7]);
-                               
-	n[1] ^= this.pass(n[0]+this.k[7]); n[0] ^= this.pass(n[1]+this.k[6]);
-	n[1] ^= this.pass(n[0]+this.k[5]); n[0] ^= this.pass(n[1]+this.k[4]);
-	n[1] ^= this.pass(n[0]+this.k[3]); n[0] ^= this.pass(n[1]+this.k[2]);
-	n[1] ^= this.pass(n[0]+this.k[1]); n[0] ^= this.pass(n[1]+this.k[0]);
- 
-	out[0] = n[1]&0xff;  out[1] = (n[1]>>>8)&0xff;
-	out[2] = (n[1]>>>16)&0xff; out[3]=n[1]>>>24; 
-	out[4] = n[0]&0xff;  out[5] = (n[0]>>>8)&0xff;
-	out[6] = (n[0]>>>16)&0xff; out[7] = n[0]>>>24;
+    n[1] ^= this.pass(n[0]+this.k[0]); n[0] ^= this.pass(n[1]+this.k[1]);
+    n[1] ^= this.pass(n[0]+this.k[2]); n[0] ^= this.pass(n[1]+this.k[3]);
+    n[1] ^= this.pass(n[0]+this.k[4]); n[0] ^= this.pass(n[1]+this.k[5]);
+    n[1] ^= this.pass(n[0]+this.k[6]); n[0] ^= this.pass(n[1]+this.k[7]);
 
+    n[1] ^= this.pass(n[0]+this.k[0]); n[0] ^= this.pass(n[1]+this.k[1]);
+    n[1] ^= this.pass(n[0]+this.k[2]); n[0] ^= this.pass(n[1]+this.k[3]);
+    n[1] ^= this.pass(n[0]+this.k[4]); n[0] ^= this.pass(n[1]+this.k[5]);
+    n[1] ^= this.pass(n[0]+this.k[6]); n[0] ^= this.pass(n[1]+this.k[7]);
+
+    n[1] ^= this.pass(n[0]+this.k[0]); n[0] ^= this.pass(n[1]+this.k[1]);
+    n[1] ^= this.pass(n[0]+this.k[2]); n[0] ^= this.pass(n[1]+this.k[3]);
+    n[1] ^= this.pass(n[0]+this.k[4]); n[0] ^= this.pass(n[1]+this.k[5]);
+    n[1] ^= this.pass(n[0]+this.k[6]); n[0] ^= this.pass(n[1]+this.k[7]);
+
+    n[1] ^= this.pass(n[0]+this.k[7]); n[0] ^= this.pass(n[1]+this.k[6]);
+    n[1] ^= this.pass(n[0]+this.k[5]); n[0] ^= this.pass(n[1]+this.k[4]);
+    n[1] ^= this.pass(n[0]+this.k[3]); n[0] ^= this.pass(n[1]+this.k[2]);
+    n[1] ^= this.pass(n[0]+this.k[1]); n[0] ^= this.pass(n[1]+this.k[0]);
+
+    out[0] = n[1]&0xff;  out[1] = (n[1]>>>8)&0xff;
+    out[2] = (n[1]>>>16)&0xff; out[3]=n[1]>>>24;
+    out[4] = n[0]&0xff;  out[5] = (n[0]>>>8)&0xff;
+    out[6] = (n[0]>>>16)&0xff; out[7] = n[0]>>>24;
 };
 
 Gost.prototype.crypt64_cfb = function (iv, clear, out) {
@@ -130,11 +129,10 @@ Gost.prototype.crypt = function (clear, out) {
 };
 
 Gost.prototype.key = function (k) {
-    var i, j; 
-	for(i=0,j=0; i<8; i++,j+=4) 
-	{
-		this.k[i] = k[j] | (k[j+1]<<8) | (k[j+2]<<16) | (k[j+3]<<24);
-	}
+    var i, j;
+    for(i=0,j=0; i<8; i++,j+=4)  {
+        this.k[i] = k[j] | (k[j+1]<<8) | (k[j+2]<<16) | (k[j+3]<<24);
+    }
 };
 
 var gost_init = function (sbox) {
