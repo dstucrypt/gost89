daa1ac9734e4b362c1eac42ebf74a3bde0f9c769
Ilya Petrov <ilya.muromec@gmail.com>
2014-11-29 21:09:25 +0700

    packaging fixes

--- index.js
+++ index.js
@@ -2,14 +2,10 @@
 
 var Gost = require('./lib/gost89.js');
 var Dstu = require('./lib/dstu.js');
-
-var gost_init = function (sbox) {
-    if (sbox === undefined) {
-        sbox = Dstu.defaultSbox;
-    }
-    return new Gost(sbox);
-};
+var Hash = require('./lib/hash.js');
 
 module.exports = {
-    init: gost_init,
+    init: Gost.init,
+    Hash: Hash,
+    gosthash: Hash.gosthash,
 };
