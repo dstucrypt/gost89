baf78a44f7250cd6349827b0a4bb308f9414f145
Ilya Petrov <ilya.muromec@gmail.com>
2014-11-29 23:59:34 +0700

    add dumb kdf

--- index.js
+++ index.js
@@ -4,8 +4,11 @@ var Gost = require('./lib/gost89.js');
 var Dstu = require('./lib/dstu.js');
 var Hash = require('./lib/hash.js');
 
+var util = require('./lib/util.js');
+
 module.exports = {
     init: Gost.init,
     Hash: Hash,
     gosthash: Hash.gosthash,
+    dumb_kdf: util.dumb_kdf,
 };
