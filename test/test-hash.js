03cc8cede72b1f66e83ca9aa72aa53613471ce30
Ilya Petrov <ilya.muromec@gmail.com>
2014-11-30 10:52:31 +0700

    fix memory madness

--- test/test-hash.js
+++ test/test-hash.js
@@ -40,5 +40,13 @@ describe('Hash', function() {
 
         });
 
+        it('should hash long chunks', function () {
+            var expect_ret = 'e9312d13d0e0d39aa4a00c77dcfb661883dd0ed8218af48a146168fc60f014dc';
+            var input = 'IGNORE THIS FILE. This file does nothing, contains no useful data, and might go away in future releases.  Do not depend on this file or its contents.';
+            var ret = gost89.gosthash(input);
+            assert.equal(ret.toString('hex'), expect_ret);
+
+        });
+
     });
 });
